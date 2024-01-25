59e469886ec5f58d7e4a48522a9c898bccbdca28
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2024-01-25 01:04:05 +0000

    Add about app with about.html and bootstrap style

--- hairbybudu/settings.py
+++ hairbybudu/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'home',
+    'about',
     'booking',
 ]
 
--- hairbybudu/urls.py
+++ hairbybudu/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path("", include("booking.urls"), name="booking-urls"),
+    path("about/", include("about.urls"), name="about-urls"),
+    path("admin/", admin.site.urls),
+    path("summernote/", include('django_summernote.urls')),
+    path("booking/", include("booking.urls"), name="booking-urls"),
     path("", include("home.urls"), name="home-urls"),
 ]
