1480fdaf31d1f9ba596e9864b2781b8deb71b847
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2024-01-26 03:11:01 +0000

    Enable serving of image files

--- about/models.py
+++ about/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
