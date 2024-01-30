4dbcc6c1ab458c2ced9cf0d0363d0aa3f80eafa0
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2024-01-30 16:18:41 +0000

    Change the booking structure and ready for deploy

--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import Booking
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import BookingForm
 from .models import Booking
+from .forms import BookingForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def book_service(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
@@ -13,17 +12,17 @@ def book_service(request):
             booking = form.save(commit=False)
             booking.user = request.user
             booking.save()
-
             messages.success(request, 'Booking successful! Thank you.')
             return redirect('booking_success')
     else:
         form = BookingForm()
-
     return render(request, 'booking/book_service.html', {'form': form})
 
+@login_required
 def booking_success(request):
     return render(request, 'booking/booking_success.html')
 
+@login_required
 def booking_detail(request, pk):
-    booking = get_object_or_404(Booking, pk=pk)
+    booking = get_object_or_404(Booking, pk=pk, user=request.user)
     return render(request, 'booking/booking_detail.html', {'booking': booking})
--- hairbybudu/settings.py
+++ hairbybudu/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-empzsolt-myfourthprojec-etsimsau93v.ws-eu107.gitpod.io',
                 '.herokuapp.com',]
